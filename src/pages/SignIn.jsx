549ecf8e1e85f82169441ce7feb18ee8ebaa20c0
Alex Sherman <asherman.ca@gmail.com>
2021-12-28 14:48:06 -0800

    o auth component and wiring

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import { toast } from 'react-toastify'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import OAuth from '../components/OAuth'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -89,7 +90,7 @@ function SignIn() {
           </div>
         </form>
 
-        {/* Google OAuth */}
+        <OAuth />
 
         <Link to='/sign-up' className='registerLink'>
           Sign Up Instead
