6c8c20992f3218117549223f409eae9e2c620aad
Alex Sherman <asherman.ca@gmail.com>
2022-01-14 16:10:41 -0800

    switched geocode api to .env file

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -89,7 +89,7 @@ function CreateListing() {
 
     if (geolocationEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyAQTfdMuKhViq_vgdAD8vYONm4vnFZkIk4`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
       )
 
       const data = await response.json()
